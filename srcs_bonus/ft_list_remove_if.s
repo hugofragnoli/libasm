52198d46100642b855d5cdae52fc25d43dd5412b
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-06 00:00:01 +0100

    maj init et clean list_remove if

--- srcs_bonus/ft_list_remove_if.s
+++ srcs_bonus/ft_list_remove_if.s
@@ -17,7 +17,14 @@ _ft_list_remove_if:
     push r13                ; "data_ref" + 8
     push r14                ; "cmp" + 8
     push r15                ; "free" + 8
-    sub rsp, 8              ; on retire 8 octets pour aligner la pile (56 + 8 = 64 donc multiple de 16)
+    push r9                 ; "previous" + 8 = 64 -> multiple de 16 donc ok pour alignement 
+
+    ; ON ATTRIBUE LES VALEURS AUX REGISTRES INIT PRECEDEMMENT
+    mov r12, rdi            ; 
+    mov r13, rsi            ;
+    mov r14, rdx            ;
+    mov r15, rcx            ;
+ 
 
 .keep_node:                 ; Pas dans le if donc on avance normalement.
     mov rbp, rbx            ; previous = current
@@ -25,7 +32,7 @@ _ft_list_remove_if:
     jmp .loop
 
 .clean
-    add rsp, 8              ;
+    pop r9             ;
     pop r15                 ;
     pop r14                 ;
     pop r13                 ;
